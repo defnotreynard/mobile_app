2178726b82da7ffe05942609e7bb7d2b7b66c12f
Reynard <your.email@example.com>
2025-08-28 21:34:55 +0800

    db connect

---
+++ assets/php/db connect/connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";   // usually "localhost"
+$username   = "root";        // your DB username
+$password   = "";            // your DB password
+$dbname     = "pole_data";     // your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("❌ Connection failed: " . $conn->connect_error);
+} else {
+  echo "✅ Connected successfully";
+}
